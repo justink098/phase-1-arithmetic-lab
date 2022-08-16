3aa06e3ef326871c2b1c1c94ffb4984aca5fab66
Justin <justin.kiarie@student.moringaschool.com>
2022-08-16 21:15:25 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+
+const random = Math.floor(Math.random() * 10) + 1;
+ 
+const num3 = 22;
+const num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max(11,20,12,13,14,15,16)
